cb48b0537a37b5d4091c22673343f2c91ec5c75e
Michael Gurov <i.gourov@gmail.com>
2025-06-02 21:38:33 +0200

    eliminate error messages

--- scripts/stash-pop.sh
+++ scripts/stash-pop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mv /tmp/nginx-proxy/configs/* /var/nginx-proxy/configs/
+mv /tmp/nginx-proxy/configs/* /var/nginx-proxy/configs/ 2>/dev/null || true
 
 # restore network attachments
 
 NAMESPACES=/var/nginx-proxy/networks
 
 # file not exists
-[[ ! -f "${NAMESPACES}" ]] && exit
+[[ ! -f "${NAMESPACES}" ]] && exit 0
 # file contains no entries
-grep -q '[^[:space:]]' "$NAMESPACES" || exit
+grep -q '[^[:space:]]' "$NAMESPACES" || exit 0
 
 # Read networks and connect nginx-proxy to each
 while IFS= read -r network; do
--- scripts/stash.sh
+++ scripts/stash.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /tmp/nginx-proxy/configs/
 rm -rf /tmp/nginx-proxy/configs/*
-mv /var/nginx-proxy/configs/* /tmp/nginx-proxy/configs/
+mv /var/nginx-proxy/configs/* /tmp/nginx-proxy/configs/ 2>/dev/null || true
 rm -rf /var/nginx-proxy/configs/*
